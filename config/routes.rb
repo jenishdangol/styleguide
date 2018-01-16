fd7f68874fcf85c3cf72eb90b9f2a88f764fa895
jenish <dangol.apple@gmail.com>
2018-01-16 10:26:30 +0545

    added slim-rails gems

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,9 @@ Rails.application.routes.draw do
   
   #setting the index page
   root 'pages#home'
-
-  get 'pages/about'
-
-  get 'pages/home'
-
-  get 'pages/contact'
+  
+  get '/about' => 'pages#about'
+  get '/contact' => 'pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
