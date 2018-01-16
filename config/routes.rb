a7486d93f4e61ab33960129d3e13e29014e949f9
jenish <dangol.apple@gmail.com>
2018-01-16 10:04:34 +0545

    created new project styleguide

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  
+  #setting the index page
+  root 'pages#home'
+
+  get 'pages/about'
+
+  get 'pages/home'
+
+  get 'pages/contact'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
